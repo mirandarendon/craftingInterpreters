147e2b43e8f514f068fed5c3ebe14708e542c073
Miranda Rendon <rendonmiranda05@gmail.com>
2026-09-17 19:00:56 -0700

    ch 7 challenge implementations

--- challenges/lox/com/craftinginterpreters/lox/Interpreter.java
+++ challenges/lox/com/craftinginterpreters/lox/Interpreter.java
@@ -96,16 +96,19 @@ class Interpreter implements Expr.Visitor<Object> {
             case PLUS:
                 if (left instanceof Double && right instanceof Double) {
                     return (double)left + (double)right;
-                } 
+                }
 
-                if (left instanceof String && right instanceof String) {
-                    return (String)left + (String)right;
+                if (left instanceof String || right instanceof String) {
+                    return stringify(left) + stringify(right);
                 }
 
                 throw new RuntimeError(expr.operator,
             "Operands must be two numbers or two strings.");
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
+                if ((double)right == 0) {
+                    throw new RuntimeError(expr.operator, "Divide by zero.");
+                }
                 return (double)left / (double)right;
             case STAR:
                 checkNumberOperands(expr.operator, left, right);
